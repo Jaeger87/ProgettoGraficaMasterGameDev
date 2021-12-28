c3cacee7174099c2c5f52b45f3803eba4aa6e25f
Jaeger <rosati.1595834@gmail.com>
2021-12-28 23:53:49 +0100

    Tutorial sprite gattino

---
+++ Tutorial/Direct3D UWP DR/Direct3D UWP DR/pch.h
@@ -0,0 +1,94 @@
+﻿//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+
+#include "BufferHelpers.h"
+#include "CommonStates.h"
+#include "DDSTextureLoader.h"
+#include "DirectXHelpers.h"
+#include "Effects.h"
+#include "GamePad.h"
+#include "GeometricPrimitive.h"
+#include "GraphicsMemory.h"
+#include "Keyboard.h"
+#include "Model.h"
+#include "Mouse.h"
+#include "PostProcess.h"
+#include "PrimitiveBatch.h"
+#include "ScreenGrab.h"
+#include "SimpleMath.h"
+#include "SpriteBatch.h"
+#include "SpriteFont.h"
+#include "VertexTypes.h"
+#include "WICTextureLoader.h"
+
+// Use the C++ standard templated min/max
+#define NOMINMAX
+
+#include <wrl/client.h>
+
+#include <d3d11_3.h>
+#include <dxgi1_6.h>
+
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <cwchar>
+#include <exception>
+#include <future>
+#include <iterator>
+#include <memory>
+#include <stdexcept>
+
+#include <pix.h>
+
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
+
+#include "winrt/Windows.ApplicationModel.h"
+#include "winrt/Windows.ApplicationModel.Core.h"
+#include "winrt/Windows.ApplicationModel.Activation.h"
+#include "winrt/Windows.Foundation.h"
+#include "winrt/Windows.Graphics.Display.h"
+#include "winrt/Windows.System.h"
+#include "winrt/Windows.UI.Core.h"
+#include "winrt/Windows.UI.Input.h"
+#include "winrt/Windows.UI.ViewManagement.h"
+
+namespace DX
+{
+    // Helper class for COM exceptions
+    class com_exception : public std::exception
+    {
+    public:
+        com_exception(HRESULT hr) noexcept : result(hr) {}
+
+        const char* what() const override
+        {
+            static char s_str[64] = {};
+            sprintf_s(s_str, "Failure with HRESULT of %08X", static_cast<unsigned int>(result));
+            return s_str;
+        }
+
+    private:
+        HRESULT result;
+    };
+
+    // Helper utility converts D3D API failures into exceptions.
+    inline void ThrowIfFailed(HRESULT hr)
+    {
+        if (FAILED(hr))
+        {
+            throw com_exception(hr);
+        }
+    }
+}
